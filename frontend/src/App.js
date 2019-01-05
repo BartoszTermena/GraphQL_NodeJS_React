39ea83947c1abfc581f88d98262bdd3cbc0439b1
bartek <Bartek.NNAAN@wp.pl>
2019-01-05 14:15:02 +0100

    Add frontend reactJS by create-react-app, first pages, components

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import './App.css';
+import Auth from './pages/Auth';
+import Events from './pages/Events';
+import Bookings from './pages/Bookings';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <Switch>
+          <Redirect from="/" to="/auth" exact />
+          <Route path="/auth" component={Auth}/>
+          <Route path="/events" component={Events}/>
+          <Route path="/bookings" component={Bookings}/>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
