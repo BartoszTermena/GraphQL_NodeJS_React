7981d705fc57cf8596e20156344d95f374b7c4e9
bartek <Bartek.NNAAN@wp.pl>
2019-01-13 10:52:16 +0100

    login, submit, creating users in mongodb

--- frontend/src/components/pages/Auth.js
+++ frontend/src/components/pages/Auth.js
@@ -3,12 +3,22 @@ import "./Auth.css";
 
 export default class Auth extends Component {
 
+  state = {
+    isLogin: true
+  }
+
   constructor(props) {
     super(props);
     this.emailEl = React.createRef();
     this.passwordEl = React.createRef();
   }
   
+  switchModeHandler = () => {
+    this.setState(prevState => {
+      return {isLogin: !prevState.isLogin};
+    })
+  }
+
   submitHandler = (e) => {
     e.preventDefault();
     const email = this.emailEl.current.value;
@@ -17,14 +27,55 @@ export default class Auth extends Component {
     if (email.trim().length === 0 || password.trim().length === 0) {
       return;
     }
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    };
+
+    if (!this.state.isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: {
+              email: "${email}",
+              password: "${password}"
+            }) {
+              _id
+              email
+            }
+          }
+        `
+      };
+    }
     
-    fetch('https://localhost:8000/graphql', {
+    
+
+    fetch('http://localhost:8000/graphql', {
       method: 'POST',
-      body: null,
-      headers: {
+      body: JSON.stringify(requestBody),
+       headers: {
         'Content-Type': 'application/json'
       }
-    });
+    })
+    .then(res => {
+      if (res.status !== 200 && res.status !== 201) {
+        throw new Error('Failed');
+      }
+      return res.json();
+    })
+    .then(resData => {
+      console.log(resData)
+    })
+    .catch(err => {
+      throw err;
+    })
   };
   render() {
     return (
@@ -41,7 +92,7 @@ export default class Auth extends Component {
 
         <div className="form-actions">
           <button type="submit">Submit</button>
-          <button type="button">Switch to Signup</button>
+          <button type="button" onClick={this.switchModeHandler}>Switch to {this.state.isLogin ? 'Signup' : 'Login'}</button>
         </div>
       </form>
     )
